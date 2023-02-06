92a873628165ad78f906324e34de33e7f9db41c2
JHole123 <15HoldenJ@harveygs.kent.sch.uk>
2023-02-06 13:07:18 +0000

    Added in Cross Hatch Scanning

--- SudokuEngine/BoardAnalyser.cs
+++ SudokuEngine/BoardAnalyser.cs
@@ -10,9 +10,45 @@ public class BoardAnalyser
             if (t.Candidates.Count == 1) return new Move("Sole Candidate", t[0], t.TileID);
         }
 
-        return new Move("Sole Candidate", board.Tiles[0][0], board.Tiles[0].TileID);
+        // cross hatch scanning
+        foreach (Segment seg in board.Segments)
+        {
+            var arg = IsFoundOnceSuper(ref board, seg, out int val);
+            if (arg != -1) return new Move("Cross Hatch Scan", val, arg);
+        }
+
+        // if no move is found, return that no move is found
+        return new Move("No Analytical Move", board.Tiles[0][0], board.Tiles[0].TileID);
     }
 
-
+    // Checks to see if there is only one instance of every candidate in a segment
+    private int IsFoundOnceSuper(ref Board board, Segment seg, out int value)
+    {
+        value = -1;
+        int tileID = -1; // -1 meaning no tile found
+        for (int i = 1; i < 10; i++) {
+            if (IsFoundOnce(ref board, seg, i))
+            {
+                // go through each tile in the segment to find which tile has the candidate
+                foreach (int tileId in seg.Tiles)
+                {
+                    if (board[tileId].Candidates.Contains(i)) { tileID = tileId; value = board[tileID].Value; break; }
+                }
+                break;
+            }
+        }
+        return tileID;
+    }
+        
+    // Checks to see if there is only one instance of a given candidate in a segment
+    private bool IsFoundOnce(ref Board board, Segment seg, int candidate)
+    {
+        int instances = 0;
+        foreach (int tileID in seg.Tiles)
+        {
+            if (board[tileID].Candidates.Contains(candidate)) instances++;
+        }
+        return instances == 1;
+    }
 }
 
--- SudokuEngine/HintsHandler.cs
+++ SudokuEngine/HintsHandler.cs
@@ -15,8 +15,11 @@ public class HintsHandler
     }
     public void PopulateDictionary()
     {
-        foreach (string s in File.ReadAllLines(@"M:\Pogramming\SudokuApplication\SudokuEngine\Hints.txt"))
-            HintsMeaning.Add(s.Split(":")[0], s.Split(":")[1]);
+        HintsMeaning.Add("Sole Candidate", "On your board, there is at least one tile with only one candidate. This means the value of that tile must be equal to the candidate. This is the simplest form of finding a new value.");
+        HintsMeaning.Add("No Analytical Move", "On your board, there are no moves that could be reasonably found using analysis.");
+        HintsMeaning.Add("Cross Hatch Scan", "some text here");
+        //foreach (string s in File.ReadAllLines(@"M:\Pogramming\SudokuApplication\SudokuEngine\Hints.txt"))
+        //    HintsMeaning.Add(s.Split(":")[0], s.Split(":")[1]);
     }
 }
 
